87704b066d4bed1c8678587f8db88eb1ffdc0cd3
rodanAsh <aakashash2002@gmail.com>
2025-07-12 10:19:58 +0530

    feat: created trip route and a form to create a new trip

---
+++ src/app/trips/page.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { auth } from "../../../auth"
+import { Button } from "@/components/ui/button";
+
+export default async function TripsPage() {
+    const session = await auth();
+    
+    if (!session) {
+        return (
+            <div className="flex justify-center items-center h-screen text-gray-700 text-xl">
+                Please Sign In.
+            </div>
+        )
+    }
+
+    return (
+        <div className="space-y-6 container mx-auto px-4 py-0">
+            <div>
+                <h1>Dashboard</h1>
+                <Link href={"/trips/new"}>
+                    <Button>
+                        New Trip
+                    </Button>
+                </Link>
+            </div>
+        </div>
+    )
+}
